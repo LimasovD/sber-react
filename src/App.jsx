131bab865eb467df357f0ff477033a66b122c8e0
Dmitriy Limasov <dv.limasov@gmail.com>
2024-05-31 17:57:25 +0300

    clean default settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Logo from "./assets/logo.svg";
 
 export default () => {
     return <>
         <h1>
-            Simple React App
-            <img src={Logo} alt="React"/>
+            Hello from React
         </h1>
     </>
 }
